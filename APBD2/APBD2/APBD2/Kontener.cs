0ec1e36037c15156ac3558a76bbbce970d97ac47
Piotr Suwara <s28023@pjwstk.edu.pl>
2024-03-22 15:32:59 +0100

    KontenerClass

---
+++ APBD2/APBD2/APBD2/Kontener.cs
@@ -0,0 +1,51 @@
+﻿namespace APBD2;
+
+public class Kontener
+{
+    // masa ładunku w kilogramach
+    private int _masaLadunku;
+    // wysokośc kontenera w centymetrach
+    private int _wysokosc;
+    // masa kontenera w kilogramach
+    private int _masaSamegoKontenera;
+    // głębokość kontenera w centymetrach
+    private int _glebokoscKontenera;
+    // maksymalna ładowność kontenera w kilogramach
+    private int _maksymalnaLadownoscKontenera;
+    // numer seryjny kontenera 
+    private static int _numerSeryjnyKontenera = 0;
+    // konstruktor kontenera 
+    public Kontener(int wysokosc, int masaSamegoKontenera, int glebokoscKontenera, int maksymalnaLadownoscKontenera)
+    {
+        _wysokosc = wysokosc;
+        _masaSamegoKontenera = masaSamegoKontenera;
+        _glebokoscKontenera = glebokoscKontenera;
+        _maksymalnaLadownoscKontenera = maksymalnaLadownoscKontenera;
+        _numerSeryjnyKontenera++;
+    }
+
+    public int GetWysokosc()
+    {
+        return _wysokosc;
+    }
+
+    public int GetMasaSamegoKontenera()
+    {
+        return _masaSamegoKontenera;
+    }
+
+    public int GetGlebokoscKontenera()
+    {
+        return _glebokoscKontenera;
+    }
+
+    public int GetMaksymalnaLadownoscKontenera()
+    {
+        return _maksymalnaLadownoscKontenera;
+    }
+
+    public int GetNumerSeryjnyKontenera()
+    {
+        return _numerSeryjnyKontenera;
+    }
+}
---
+++ APBD2/APBD2/APBD2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace APBD2;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Kontener kontener = new Kontener(10, 10, 10, 10);
+            Console.WriteLine(kontener.GetNumerSeryjnyKontenera());
+        }
+    }
+}
